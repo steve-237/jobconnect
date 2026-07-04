e174f7b3245ad4a81dd1dc17734dea7f4aebc1a2
steve-237 <steve0peg@gmail.com>
2026-07-05 01:17:50 +0200

    feat(backend): generate CRUD resources for users, jobs, and categories

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { JobsModule } from './jobs/jobs.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, JobsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
